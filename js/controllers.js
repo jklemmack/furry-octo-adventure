d80af7a5d41bbebf49a878b4babe5294a461a0ba
Johann Klemmack <johann.klemmack@gmail.com>
2013-07-19 20:58:32 -0700

    cleaned out more phonegap specific stuff

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,17 @@
 function PhoneListCtrl($scope) {
-  $scope.phones = [
-    {"name": "Nexus S",
-     "snippet": "Fast just got faster with Nexus S."},
-    {"name": "Motorola XOOM™ with Wi-Fi",
-     "snippet": "The Next, Next Generation tablet."},
-    {"name": "MOTOROLA XOOM™",
-     "snippet": "The Next, Next Generation tablet."}
-  ];
+  $.get('data.js', function(x) {
+					$scope.phones = JSON.parse(x);
+				})
+				.fail(function(e) {
+					alert('fail');
+				});
+	
+//	$scope.phones = [
+//    {"name": "Nexus S",
+//     "snippet": "Fast just got faster with Nexus S."},
+//    {"name": "Motorola XOOM™ with Wi-Fi",
+//     "snippet": "The Next, Next Generation tablet."},
+//    {"name": "MOTOROLA XOOM™",
+//     "snippet": "The Next, Next Generation tablet."}
+//  ];
 }
